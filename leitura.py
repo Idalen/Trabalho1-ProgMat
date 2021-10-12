6d0fe745a681c837763dcc2016717df59a65df2f
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-10-12 20:00:10 -0300

    Update leitura.py

--- leitura.py
+++ leitura.py
@@ -14,41 +14,52 @@
 #   iN  .......................
 
 import numpy as np
+import time
+from hungarian import *
+from gurobi import *
+from cbc import *
 
 # Dados sobre a formatacao dos nomes dos arquivos
-pastas = ["a", "b"]
+pastas = ["a","b"]
 u = [2,3]
 v = [1,2,3,4,5,6,7,8,9,10]
 n = [50,100,200,500,1000]
 
-# Leitura de tarefas/agentes (TALVEZ SEJA DESNECESSARIO)
-# for tarefas_agentes in n: # Range para as variacoes de 50, 100, 200...
-#     nome_arquivo = "_" + str(tarefas_agentes) + ".txt"
-#     print("--------------------INICIO DE inst" + nome_arquivo + "---------------------")
-            
-#     # Faca a magica aqui...
-#     tarefas_agentes = np.loadtxt("./insta/inst_1" + nome_arquivo)
-            
-#     print("--------------------FIM DE inst" + nome_arquivo + "------------------------")
-
-
+all_times = np.array([])
 
 # Leitura dos dados
 for tipo_pasta in pastas: # Range para cada pasta, no caso pasta A e B
     for tipo_lucro in u: # Range para as variacoes de 2 e 3
-        
         # Se tipo_lucro eh 2 entao lucro (cada item da matriz) esta no intervalo de [5, 50]
         # Se tipo_lucro eh 3 entao lucro (cada item da matriz) esta no intervalo de [5, 500]
-        
         for instancia in v: # Range para as variacoes de 1,2,3,4,5...
             for tarefas_agentes in n: # Range para as variacoes de 50, 100, 200...
-                nome_arquivo = "inst" + tipo_pasta + "_" + str(tipo_lucro) + "_" + str(instancia) + "_" + str(tarefas_agentes) + ".txt"
-                print("--------------------INICIO DE " + nome_arquivo + "---------------------")
+                print("\n--------------------INICIO DE inst"+tipo_pasta+"_"+str(tipo_lucro)+"_"+str(instancia)+"_"+ str(tarefas_agentes) +".txt---------------------\n")
                 
                 # Carregamento do arquivo para matriz_lida com numpy.loadtxt()
-                matriz_lida = np.loadtxt("./inst" + tipo_pasta + "/" + nome_arquivo)
+                matriz_lida = np.loadtxt("./inst"+tipo_pasta+"/inst"+tipo_pasta+"_"+str(tipo_lucro)+"_"+str(instancia)+"_"+ str(tarefas_agentes) +".txt")
                 
                 # Faca a magica aqui...
-                # print(matriz_lida)
                 
-                print("--------------------FIM DE " + nome_arquivo + "------------------------")
+                start = time.time()
+                
+                # hungarianMethod(matriz_lida)
+                guropi(matriz_lida)
+                # cbc2(matriz_lida)
+                
+                end = time.time()
+                
+                # Calcula o tempo gasto nessa execucao e salva em all_times
+                curr_time = end - start           
+                all_times = np.append(all_times, curr_time)
+                
+                print(f"Runtime is {curr_time}")
+
+# print("Todos os tempos: ", all_times)
+
+for index, tarefas_agentes in enumerate(n):
+    # print("Resultados dos arquivos com tamanho "+str(tarefas_agentes)+": ", all_times[index::len(n)])
+    print("Tempo medio dos arquivos com tamanho "+str(tarefas_agentes)+": ", np.mean(all_times[index::len(n)])) 
+    # Ele faz um skip do index dado, com step do tamanho de len
+    # Isso eh necessario para termos a media de cada tamanho de arquivo
+    print("\n")
